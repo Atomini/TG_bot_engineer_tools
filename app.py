f409595db60ade0eac05fc6b0d699edf389b22f9
Atomini <62155406+Atomini@users.noreply.github.com>
2021-01-13 21:41:57 +0200

    Написанна основа бота

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import asyncio
+from config import TOKEN
+from aiogram import Bot, Dispatcher, executor, types
+import logging
 
-
+loop = asyncio.get_event_loop()
+bot = Bot(token=TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot, loop=loop)
 
 
 if __name__ == '__main__':
-    print("aaaaaaa")
+    from heandlers import dp, send_to_admin
+    executor.start_polling(dp, on_startup=send_to_admin, skip_updates=True)

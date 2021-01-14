69f0b0397bebd30f7642be6e32e00c4d3de4c592
Atomini <RetRot@fex.net>
2021-01-14 16:25:11 +0200

    Добавил requirements.txt

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from config import TOKEN
 from aiogram import Bot, Dispatcher, executor, types
 import logging
 
+logging.basicConfig(level=logging.DEBUG)
 loop = asyncio.get_event_loop()
 bot = Bot(token=TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, loop=loop)
 
-
 if __name__ == '__main__':
     from heandlers import dp, send_to_admin
+
     executor.start_polling(dp, on_startup=send_to_admin, skip_updates=True)
